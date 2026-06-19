4f2856b571d307b88bf575fea004e7fdd83e7be0
wenyilaw <wenyi.law@ytlcomms.my>
2026-06-19 16:36:41 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import SIPPricingPlaybook from './SIPPricingPlaybook.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SIPPricingPlaybook />
+  </React.StrictMode>
+);
